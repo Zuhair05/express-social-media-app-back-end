41d4213685f37308f7c258412814a48f8020471b
Zuhair05 <alshabaq98@gmail.com>
2026-08-09 12:39:04 +0300

    update controller

--- controllers/post.js
+++ controllers/post.js
@@ -30,3 +30,25 @@ const show = async (req, res) => {
     }
 }
 
+const update = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId)
+
+    if (!post.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!")
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      req.body,
+      { new: true }
+    )
+
+    updatedPost._doc.author = req.user
+
+    res.status(200).json(updatedPost)
+  } catch (err) {
+    res.status(500).json({ err: err.message })
+  }
+}
+
