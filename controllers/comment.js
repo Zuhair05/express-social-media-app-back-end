b2c8c445eed7681eda87f31fe4d28270f2c5b5f0
Zuhair05 <alshabaq98@gmail.com>
2026-08-09 12:53:22 +0300

    index  controller in comment

--- controllers/comment.js
+++ controllers/comment.js
@@ -14,5 +14,14 @@ const create = async (req, res) => {
     }
 }
 
-
+const index = async (req, res) => {
+    try {
+        const comments = await Comment.find({ post: req.params.postId })
+            .populate('author', 'username')
+            .sort({ createdAt: "desc" });
+        res.status(200).json(comments);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+}
 
