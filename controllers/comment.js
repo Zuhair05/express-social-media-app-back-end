0d072e03dd7c7eb388cfe9e5d5c5ff4db06ff84e
Zuhair05 <alshabaq98@gmail.com>
2026-08-09 14:31:10 +0300

    Amendment on update and delete controller on comment

--- controllers/comment.js
+++ controllers/comment.js
@@ -27,17 +27,46 @@ const index = async (req, res) => {
 
 const update = async (req, res) => {
     try {
-        const post = await Post.findById(req.params.postId);
+        const comment = await Comment.findById(req.params.commentId)
+
+        if(!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+    
+        if (comment.author.toString() !== req.user._id.toString()) {
+            return res.status(403).json({message: "You're not allowed to do that!"})
+        }
+        comment.text = req.body.text
+        await comment.save()
+        res.status(200).json({message: 'Comment updated successfully',comment})
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+}
+
+const deleteComment = async (req, res) => {
+    try {
         const comment = await Comment.findById(req.params.commentId);
 
-        if (comment.author.toString() !== req.user._id) {
-            return res.status(403).send("You're not allowed to do that!");
+        if(!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
         }
 
-        comment.text = req.body.text;
-        await comment.save();
-        res.status(200).json({ message: 'Comment updated successfully', comment });
-    } catch (err) {
+        if (comment.author.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                message: "You're not allowed to do that!"
+            })
+        }
+        await comment.deleteOne();
+        res.status(200).json({ message: 'Comment deleted successfully' });
+    }catch (err) {
         res.status(500).json({ err: err.message });
     }
+}
+
+module.exports = {
+    create,
+    index,
+    update,
+    deleteComment,
 }
